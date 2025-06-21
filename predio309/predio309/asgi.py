55070c5b2f8d72cfbdfa5e497eeb4c5b3633ed1c
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2025-06-21 13:57:10 -0300

    first commit

---
+++ predio309/predio309/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for predio309 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'predio309.settings')
+
+application = get_asgi_application()
